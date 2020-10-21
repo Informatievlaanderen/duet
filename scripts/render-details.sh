fd67242777f1a4dcea007032086c740501bda7c1
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2020-10-21 18:59:03 +0000

    add multilingual shacl

--- scripts/render-details.sh
+++ scripts/render-details.sh
@@ -113,22 +113,22 @@ render_html() { # SLINE TLINE JSON
             echo "RENDER-DETAILS(html): File was created in ${TLINE}/index.html"
         fi
 
-#        OUTPUT=${TLINE}/index_${GOALLANGUAGE}.html
-#        COMMANDTEMPLATELANG=$(echo '.[].translation | .[] | select(.language | contains("'${GOALLANGUAGE}'")) | .template')
-#        TEMPLATELANG=$(jq -r "${COMMANDTEMPLATELANG}" ${SLINE}/.names.json)
-#        MERGEFILENAME=$(jq -r ".name" ${JSONI})_${GOALLANGUAGE}
-#        COMMANDJSONLD=$(echo '.[].translation | .[] | select(.language | contains("'${GOALLANGUAGE}'")) | .mergefile')
-#        MERGEDJSONLD=${RRLINE}/translation/$(jq -r "${COMMANDJSONLD}" ${SLINE}/.names.json)
-#
-#        echo "RENDER-DETAILS(language html): node /app/html-generator2.js -s ${TYPE} -i ${MERGEDJSONLD} -x ${RLINE}/html-nj.json -r ${DROOT} -t ${TEMPLATELANG} -d ${SLINE}/templates -o ${OUTPUT} -m ${GOALLANGUAGE} -e ${RLINE}"
-#
-#	if ! node /app/html-generator2.js -s ${TYPE} -i ${MERGEDJSONLD} -x ${RLINE}/html-nj_${GOALLANGUAGE}.json -r ${DROOT} -t ${TEMPLATELANG} -d ${SLINE}/templates -o ${OUTPUT} -m ${GOALLANGUAGE} -e ${RRLINE} 
-#        then   
-#            echo "RENDER-DETAILS(language html): rendering failed"
-#            exit -1
-#        else
-#            echo "RENDER-DETAILS(language html): File was rendered in ${OUTPUT}"
-#        fi
+        OUTPUT=${TLINE}/index_${GOALLANGUAGE}.html
+        COMMANDTEMPLATELANG=$(echo '.[].translation | .[] | select(.language | contains("'${GOALLANGUAGE}'")) | .template')
+        TEMPLATELANG=$(jq -r "${COMMANDTEMPLATELANG}" ${SLINE}/.names.json)
+        MERGEFILENAME=$(jq -r ".name" ${JSONI})_${GOALLANGUAGE}
+        COMMANDJSONLD=$(echo '.[].translation | .[] | select(.language | contains("'${GOALLANGUAGE}'")) | .mergefile')
+        MERGEDJSONLD=${RRLINE}/translation/$(jq -r "${COMMANDJSONLD}" ${SLINE}/.names.json)
+
+        echo "RENDER-DETAILS(language html): node /app/html-generator2.js -s ${TYPE} -i ${MERGEDJSONLD} -x ${RLINE}/html-nj.json -r ${DROOT} -t ${TEMPLATELANG} -d ${SLINE}/templates -o ${OUTPUT} -m ${GOALLANGUAGE} -e ${RLINE}"
+
+	if ! node /app/html-generator2.js -s ${TYPE} -i ${MERGEDJSONLD} -x ${RLINE}/html-nj_${GOALLANGUAGE}.json -r ${DROOT} -t ${TEMPLATELANG} -d ${SLINE}/templates -o ${OUTPUT} -m ${GOALLANGUAGE} -e ${RRLINE} 
+        then   
+            echo "RENDER-DETAILS(language html): rendering failed"
+            exit -1
+        else
+            echo "RENDER-DETAILS(language html): File was rendered in ${OUTPUT}"
+        fi
         
         # make the report better readable
         jq . ${RLINE}/html-nj.json > ${RLINE}/html-nj.json2
